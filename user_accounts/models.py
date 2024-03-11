1535be72e68239a99b053969002bb445c9b49b34
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-03-11 10:33:38 -0400

    Overrode first_name in user model.

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(blank=False, max_length=150, verbose_name='first name')
---
+++ user_accounts/tests/test_user_model.py
@@ -0,0 +1,31 @@
+import random
+import string
+from django.forms import ValidationError
+from django.test import TestCase
+
+from user_accounts.models import User
+
+
+class TestUserModel(TestCase):
+
+    def setUp(self):
+        self.ascii = string.ascii_letters
+        self.user = User(username='johndoe', first_name='John', last_name='Doe',
+                        email='johndoe@example.com')
+
+    def test_first_name_not_blank(self):
+        with self.assertRaises(ValidationError) as e:
+            self.user.full_clean()
+        self.assertNotIn('first_name', e.exception.message_dict)
+    
+    def test_first_name_blank(self):
+        with self.assertRaises(ValidationError) as e:
+            self.user.first_name = None
+            self.user.full_clean()
+        self.assertIn('first_name', e.exception.message_dict)
+    
+    def test_first_name_within_150_chars(self):
+        with self.assertRaises(ValidationError) as e:
+            self.user.first_name = ''.join(random.choice(self.ascii) for _ in range(151))
+            self.user.full_clean()
+        self.assertIn('first_name', e.exception.message_dict)
