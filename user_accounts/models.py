46440632a72f3baacce160eb3a2e2c9fcb6a29c9
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-03-22 11:53:33 -0400

    Implemented CustomUserManager and added User model to admin site for super user management.

--- user_accounts/models.py
+++ user_accounts/models.py
@@ -1,14 +1,44 @@
 from datetime import date
 from django.forms import ValidationError
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, birthday, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given username, email, birthday, and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, birthday=birthday, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, birthday, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given username, email, birthday, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, birthday, password, **extra_fields)
+    
 
 class User(AbstractUser):
     first_name = models.CharField(blank=False, max_length=150, verbose_name='first name')
     last_name = models.CharField(blank=False, max_length=150, verbose_name='last name')
     email = models.EmailField(blank=False, max_length=254, verbose_name='email address')
     birthday = models.DateField(blank=False, auto_now=False, auto_now_add=False)
+
+    objects = CustomUserManager()
 
     def clean_fields(self, exclude=None) -> None:    
         super().clean_fields(exclude=exclude)
