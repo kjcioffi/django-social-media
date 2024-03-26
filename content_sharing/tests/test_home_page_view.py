3e35463376a20bdd704298c85a65ba8005174304
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-03-26 14:48:26 -0400

    Form logic for an upcoming feature in the home page view.    - Associated tests for PostForm and ensuring it functions in the view.    - Implemented initial logic for posting forms, dynamic updates will be added in later features.    - Refactored test logic to better support login / logout functionality and reduce redundancy.

--- content_sharing/tests/test_home_page_view.py
+++ content_sharing/tests/test_home_page_view.py
@@ -1,27 +1,46 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from content_sharing.models import UserProfile
 from user_accounts.models import User
 
 class TestHomePageView(TestCase):
 
+    def login_user(self, username='johndoe', password='temporary123'):
+        self.client.login(username=username, password=password)
+        self.response = self.client.get(reverse('content_sharing:home_page'))
+
     def setUp(self):
         self.client = Client()
-        self.request = self.client.get(reverse('content_sharing:home_page'))
         self.user = User.objects.create_user(username='johndoe', first_name='John',
                                         last_name='Doe', email='johndoe@example.com',
                                         birthday='1900-01-01',
                                         password='temporary123')
+        
+        self.login_user()
 
     def test_redirect_if_not_logged_in(self):
-        self.assertEqual(self.request.status_code, 302)
-        self.assertTrue(self.request.url.startswith('/login'))
+        self.client.logout()
+        self.response = self.client.get(reverse('content_sharing:home_page'))
+        self.assertEqual(self.response.status_code, 302)
+        self.assertTrue(self.response.url.startswith('/login'))
 
     def test_access_with_authenticated_user(self):
-        self.client.login(username=self.user.username, password='temporary123')
-        self.request = self.client.get(reverse('content_sharing:home_page'))
-        self.assertEqual(self.request.status_code, 200)
-        self.assertEqual(self.request.request['PATH_INFO'], '/')
+        self.assertEqual(self.response.status_code, 200)
+        self.assertEqual(self.response.request['PATH_INFO'], '/')
+
+    def test_form_in_context(self):
+        self.assertIn('form', self.response.context)
+
+    def test_form_bound_when_posted(self):
+        data={'content': 'Today is a beautiful day to go outside and enjoy the fresh air. What are your plans for today?'}
+        response = self.client.post(reverse('content_sharing:home_page'), data)
+        print(response)
+        
+    def test_form_not_bound_on_page_load(self):
+        form = self.response.context['form']
+        self.assertIsNotNone(form)
+        self.assertFalse(form.is_bound, 'Form should be empty when page loads.')
 
     def test_create_new_post(self):
         pass
--- content_sharing/views.py
+++ content_sharing/views.py
@@ -1,6 +1,18 @@
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+from content_sharing.forms import PostForm
 
 @login_required
 def homepage(request):
-    return HttpResponse()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user_profile = request.user.userprofile
+            post.save()
+    else:
+        form = PostForm()
+    
+    return render(request, 'content_sharing/homepage.html', {'form': form})
