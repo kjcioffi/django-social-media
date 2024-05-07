3844028c2523dfda2a991d1b2df1aa899c4d5bdb
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-05-07 13:08:54 -0400

    Whitespace clean up in post model testsuite.

--- content_sharing/tests/test_post_model.py
+++ content_sharing/tests/test_post_model.py
@@ -12,8 +12,6 @@ from content_sharing.models import Post, Profile
 class PostModelTest(TestCase):
 
     def setUp(self):
-
-
         self.user = User.objects.create(username='johndoe', password='j@hND03')
         self.profile = Profile.objects.create(user=self.user)
 
