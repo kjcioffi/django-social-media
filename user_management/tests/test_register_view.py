efc1c469d3749e8c964b22fc309e7c0cf181b488
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-05-11 12:35:09 -0400

    Refactor RegisterView test suite.    - Factored response object out of test and into setUp.    - Modified a portion of the redirect test as there was a username conflict after refactor.

--- user_management/tests/test_register_view.py
+++ user_management/tests/test_register_view.py
@@ -5,20 +5,22 @@ from django.contrib.auth.models import User
 
 from django.urls import reverse
 
+
 class RegisterViewTest(TestCase):
 
     def setUp(self):
         self.client = Client()
+
+        self.response = self.client.post(reverse('user_management:register'),
+                                         {'username': 'johndoe', 'password1': 'crdRBDwEGk9Cr03FcPJC',
+                                                                 'password2': 'crdRBDwEGk9Cr03FcPJC'})
 
     def test_form_in_context(self):
         request = self.client.get(reverse('user_management:register'))
         self.assertTrue(request.context['form'])
 
     def test_successful_user_creation(self):
-        response = self.client.post(reverse('user_management:register'), {'username': 'johndoe', 'password1': 'crdRBDwEGk9Cr03FcPJC',
-                                                               'password2': 'crdRBDwEGk9Cr03FcPJC'})
-        
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.response.status_code, 302)
         
         try:
             user = User.objects.filter(username='johndoe')
@@ -28,8 +30,9 @@ class RegisterViewTest(TestCase):
 
     def test_redirect_to_login(self):
         redirect = self.client.post(reverse('user_management:register'),
-                                    {'username': 'johndoe', 'password1': 'crdRBDwEGk9Cr03FcPJC',
+                                    {'username': 'maryjane', 'password1': 'crdRBDwEGk9Cr03FcPJC',
                                                                'password2': 'crdRBDwEGk9Cr03FcPJC'},
                                                                follow=True)
+        
         self.assertEqual(redirect.status_code, 200)
         self.assertRedirects(redirect, reverse('user_management:login'))
