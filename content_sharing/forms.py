0147e96567b2c68346a927b9fa51848ebfe51282
Cioffi, Kevin <cioffijr.kevin@gmail.com>
2024-05-31 11:40:41 -0400

    Refactored BioForm for current and future features.    - Refactored from ModelForm to Form because there isn't a need to save model objects.    - Added csrf_token to the form.    - Toggled required form field attribute to false so user's remove all contents of their bio if they'd like.

--- content_sharing/forms.py
+++ content_sharing/forms.py
@@ -1,19 +1,21 @@
 from django import forms
-from django.forms import ModelForm
+from django.core.validators import MaxLengthValidator
+from django.forms import Form, ModelForm
 
-from content_sharing.models import Post, Profile
+from content_sharing.models import Post
 
 
 class PostForm(ModelForm):
-    content = forms.CharField(widget=forms.Textarea({'id': 'content-to-be-posted'}))
+    content = forms.CharField(widget=forms.Textarea({"id": "content-to-be-posted"}))
 
     class Meta:
         model = Post
-        fields = ['content']
+        fields = ["content"]
 
-class BioForm(ModelForm):
-    bio = forms.CharField(widget=forms.Textarea({'id': 'bio'}))
 
-    class Meta:
-        model = Profile
-        fields = ['bio']
+class BioForm(Form):
+    bio = forms.CharField(
+        widget=forms.Textarea({"id": "bio"}),
+        validators=[MaxLengthValidator(600)],
+        required=False,
+    )
